e0602c68db829080e02a8b69156ffb52a9d6a84c
sumanta2 <sumantagorai.21@gmail.com>
2022-03-12 16:08:45 +0530

    16 vdo25 ApolloClient ApolloProvider setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import BookList from "./components/BookList";
+import {
+  ApolloClient,
+  ApolloProvider,
+} from "@apollo/client";
+
+//apollo client setup
+const client = new ApolloClient({   //here we provide the server endpoint url 
+  uri:'http://localhost:4000/graphql'
+})
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div>
-      <h1>Sumanta's Reading List</h1>
-      <BookList/>
+      <h1>Sumatra's Reading List</h1>
+      <BookList/> 
 
     </div>
+    </ApolloProvider>
   );
 }
 
