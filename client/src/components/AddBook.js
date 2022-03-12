8c349c4630115e193c22e82f393e74045456309f
sumanta2 <sumantagorai.21@gmail.com>
2022-03-12 21:22:55 +0530

    21 vdo30 collect Form data

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,50 +1,70 @@
+import {useState} from 'react'
 import {
     // gql,
     useQuery
-  } from "@apollo/client";
+} from "@apollo/client";
 
-  import {getAuthorsQuery} from '../queries/queries'
+import { getAuthorsQuery } from '../queries/queries'
 
 
-  function AddBook() {
-    const { loading,data } = useQuery(getAuthorsQuery)
+function AddBook() {
+    const [value, setValue] = useState({ name: '', genre: '', authorId: 'Not Selected' })
 
-    if(loading){return "Error..."}
+    const { loading, data } = useQuery(getAuthorsQuery)
+
+    if (loading) { return "Error..." }
     //console.log(data)
 
+    const submitForm=(e)=>{
+        e.preventDefault()
+        console.log(value)
+
+    }
+
+
+    const inputEvent=(event)=>{
+
+        const { value, name }=event.target
+        setValue((preValue)=>{
+
+            return{
+                ...preValue,   
+                [name]:value,  
+            }
+        })}
+
+
     return (
-      <>
-          <form id="add-book">
-            
-            <div className="field">
-                <label>Book name:</label>
-                <input type="text"/>
-            </div>
-            <div className="field">
-                <label>Genre:</label>
-                <input type="text"/>
-            </div>
-            <div className="field">
-                <label>Author:</label>
-                <input type="text"/>
-            </div>
+        <>
+            <form id="add-book" onSubmit={submitForm}>
 
-            <div className="field">
-                <label>Author:</label>
-                <select>
-                {loading && <option>Loading Authors</option> }
-                    
-                    {  !loading &&
-                        data?.authors.map((author)=>{
-                        return <option key={author.id} value={author.name}>{author.name}</option>
+                <div className="field">
+                    <label>Book name:</label>
+                    <input type="text" name="name" value={value.name} onChange={inputEvent} />
+                </div>
+
+                <div className="field">
+                    <label>Genre:</label>
+                    <input type="text" name="genre" value={value.genre} onChange={inputEvent} />
+                </div>
+
+                <div className="field">
+                    <label>Author:</label>
+                    <select name="authorId" onChange={inputEvent} >
+                    {loading && <option>Loading Authors</option>}
+                    <option >select author</option>
+                    {!loading &&
+                        data?.authors.map((author) => {
+                            return <option key={author.id} value={author.id}>{author.name}</option>
                         })}
-                </select>
+                    </select>
+                
             </div>
-            <button>+</button>
+            <button type="submit">+</button>
 
-          </form>
+        </form>
       </>
     );
-  }
-  
-  export default AddBook;
+}
+
+export default AddBook;
