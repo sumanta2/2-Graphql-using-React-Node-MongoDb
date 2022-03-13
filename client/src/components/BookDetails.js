a1ccdd940b2ac161b9b4037fce601fb717d65400
sumanta2 <sumantagorai.21@gmail.com>
2022-03-13 19:08:17 +0530

    26 vdo35 Render fetch Data

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,19 +1,49 @@
 import {
     useLazyQuery
 } from "@apollo/client";
+import { useEffect } from "react";
 
 import { getBookQuery } from '../queries/queries'
 
 
-function BookDetails() {
+function BookDetails({ bookId }) {
 
-    const [searchPosts, { loading: bookLoading }] = useLazyQuery(getBookQuery) //useLazyQuery hook used to send query parameter value to any query request
+    const [bookPosts, { loading: bookLoading, data: bookData, error: bookError }] = useLazyQuery(getBookQuery) //useLazyQuery hook used to send query parameter value to any query request
+
+    useEffect(() => {
+        bookPosts({
+            variables: {
+                id: bookId.selected
+            }
+        })
+
+    }, [bookId.selected])
+
+    // useEffect(() => {
+    //     console.log(bookData)
+    // }, [bookData])
+
+    var book = bookData?.book
 
     if (bookLoading) { return "Loading..." }
+    if (bookError) { return "Error in Fetching Data :( " }
+
+
+
+
 
     return (
         <div >
-            <p>Output Book Details</p>
+            <h2>{book?.name}</h2>
+            <p>{book?.genre}</p>
+            <p>{book?.author.name}</p>
+            {book && <p>All Books by this author:</p>}
+            <ul className="other-books">
+                {book?.author.books.map((item) => {
+                    return <li key={item.id}>{item.name}</li>
+                })}
+            </ul>
+            {!book && <p>No Books Found...</p>}
         </div>
     );
 }
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,32 +1,33 @@
 import {
-    // gql,
-    useQuery
-  } from "@apollo/client";
+  // gql,
+  useQuery
+} from "@apollo/client";
+import { useState } from "react";
 
-  import {getBooksQuery} from '../queries/queries'
+import { getBooksQuery } from '../queries/queries'
 import BookDetails from "./BookDetails";
 
-  
+
 function BookList() {
-    const { loading, error, data } = useQuery(getBooksQuery)
+  const [Track, setTrack] = useState({ selected: null })
+  const { loading, error, data } = useQuery(getBooksQuery)
 
-    if (loading){ return "Loading..."}
-    if(error){return "Error..."}
-    //console.log(data)
+  if (loading) { return "Loading..." }
+  if (error) { return "Error..." }
+  //console.log(data)
 
-    return (
-      <div >
-        <ul id="book-list">
-            {
-                data.bookss.map((book)=>{
-                     return <li key={book.id}>{book.name}</li>
-                    })
-            }
-        </ul>
-        <BookDetails/>
-      </div>
-    );
-  }
-  
-  export default BookList;
-  
+  return (
+    <div >
+      <ul id="book-list">
+        {
+          data.bookss.map((book) => {
+            return <li key={book.id} onClick={() => { setTrack({ selected: book.id }) }}>{book.name}</li>
+          })
+        }
+      </ul>
+      <BookDetails bookId={Track} />
+    </div>
+  );
+}
+
+export default BookList;
